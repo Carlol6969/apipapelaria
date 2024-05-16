176ca372ce1431a6b4a654e9f3b96af3a17c8202
Carlol6969 <142802832+Carlol6969@users.noreply.github.com>
2024-05-16 11:16:58 -0300

    api andando devagar

--- app.js
+++ app.js
@@ -1,5 +1,22 @@
 const express = require("express")
 const app = express();
+const usuario=[
+    {nome:"carlos", idade:"19", peso:"70"},
+    {nome:"Pedro", idade:"30", peso:"100"},
+    {nome:'Joao', idade:"40", peso:"60", cep:"77807060"},
 
-app.use((req, res, next)=>{res.send("watashi no API desu")})
+]
+
+app.use("/todos",(req, res, next)=>{res.send(usuario)})
+app.use("/nome",(req, res, next)=>{res.send(usuario[1].nome)})
+app.use("/cep",(req, res, next)=>{res.send(usuario[2].cep)})
+app.use("/soma",(req, res, next)=>{
+    let total=0
+    for(i=0; i<usuario.length; i++)
+    {
+      total=total+parseInt(usuario[i].idade)
+    }
+
+    res.send("Soma total: "+total);
+})
 module.exports = app
