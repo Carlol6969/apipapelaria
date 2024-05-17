a2d45886d7f57b5fd912e10333b9f8a49685be82
Carlol6969 <142802832+Carlol6969@users.noreply.github.com>
2024-05-17 11:05:18 -0300

    api de cep

--- app.js
+++ app.js
@@ -1,5 +1,43 @@
 const express = require("express")
 const app = express();
+const cep =[
+  {
+    "cep": "01001-001",
+    "logradouro": "Praça da Sé",
+    "complemento": "lado ímpar",
+    "bairro": "Sé",
+    "localidade": "São Paulo",
+    "uf": "SP",
+    "ibge": "3550308",
+    "gia": "1004",
+    "ddd": "11",
+    "siafi": "7107"
+  },
+  {
+    "cep": "01001-000",
+    "logradouro": "Praça da Sé",
+    "complemento": "lado ímpar",
+    "bairro": "Sé",
+    "localidade": "São Paulo",
+    "uf": "SP",
+    "ibge": "3550308",
+    "gia": "1004",
+    "ddd": "11",
+    "siafi": "7107"
+  },
+  {
+    "cep": "01001-000",
+    "logradouro": "Praça da Sé",
+    "complemento": "lado ímpar",
+    "bairro": "Sé",
+    "localidade": "São Paulo",
+    "uf": "SP",
+    "ibge": "3550308",
+    "gia": "1004",
+    "ddd": "11",
+    "siafi": "7107"
+  }
+]
 const usuario=[
     {nome:"carlos", idade:"19", peso:"70"},
     {nome:"Pedro", idade:"30", peso:"100"},
@@ -9,7 +47,9 @@ const usuario=[
 
 app.use("/todos",(req, res, next)=>{res.send(usuario)})
 app.use("/nome",(req, res, next)=>{res.send(usuario[1].nome)})
-app.use("/cep",(req, res, next)=>{res.send(usuario[2].cep)})
+// // app.use("/cep/:valor",(req, res, next)=>{
+// //   const valor=req.params.valor
+//   res.send(valor)})
 app.use("/soma",(req, res, next)=>{
     let total=0
     for(i=0; i<usuario.length; i++)
@@ -18,5 +58,17 @@ app.use("/soma",(req, res, next)=>{
     }
 
     res.send("Soma total: "+total);
+})
+
+app.use("/cep/:valor",(req, res, next)=>{
+  const valor = req.params.valor
+  const local = cep.filter(locais =>locais.cep === valor)
+
+  res.send(local)
+})
+app.use("/viacep/:valor",(req, res, next)=>{
+  const valor = req.params.valor
+fetch("https://viacep.com.br/ws/"+valor+"/json/").then(resposta=>{local})
+
 })
 module.exports = app
