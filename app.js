fdc7df35f36acd80e3191338549525ec25da699d
Carlol6969 <142802832+Carlol6969@users.noreply.github.com>
2024-05-27 11:11:02 -0300

    validações e alteraçoes na lista de usuarios

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
-const { json } = require("express");
-const express = require("express")
+const express = require('express')
 const app = express();
 app.use(express.json());
 const cors = require("cors");
@@ -8,22 +7,23 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended:false}))
 const morgan = require("morgan");
 app.use(morgan("dev"));
+
 app.use((req,res,next)=>{
- 
-  res.header("Access-Control-Allow-Origin","*");
+    res.header("Access-Control-Allow-Origin","*");
 
-  res.header(
-      "Access-Control-Allow-Headers",
-      "Origin, X-Requested-With, Content-Type, Accept,Autorization"
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept,Autorization"
 
-  );
-  if(req.method === "OPTIONS"){
-      res.header("Access-Control-Allow-Methods","PUT, POST, PATCH, DELETE, GET");
-      return res.status(200).send({});
-  }
-  next();
+    );
+    if(req.method === "OPTIONS"){
+        res.header("Access-Control-Allow-Methods","PUT, POST, PATCH, DELETE, GET");
+        return res.status(200).send({});
+    }
+    next();
 })
 const rotaUsuario = require("./routes/rotaUsuario");
+
 app.use("/usuario",rotaUsuario)
 
 module.exports = app
--- routes/rotaUsuario.js
+++ routes/rotaUsuario.js
@@ -2,46 +2,108 @@ const express = require("express");
 const router = express.Router();
 const sqlite3 = require("sqlite3").verbose();
 const db = new sqlite3.Database("database.db");
-db.run(`CREATE TABLE IF NOT EXISTS
-usuario(
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    nome TEXT,
-    email TEXT,
-    senha TEXT
-         )`,
-(createTableError) => {
-    if(createTableError){
-        return res.status(500).send({error:createTableError.message})
+db.run(`CREATE TABLE IF NOT EXISTS 
+         usuario (
+            id INTEGER PRIMARY KEY AUTOINCREMENT, 
+            nome TEXT, 
+            email TEXT, 
+            senha TEXT)
+            `, (createTableError) => {
+    if (createTableError) {
+        return res.status(500).send({
+            error: createTableError.message
+        });
     }
-}
-);
-const usuario=[{
-    id:1,
-    nome:"joao"
-},
-{
-    id:2,
-    nome:"pedro"
-}]
+});
 
+
+//consultar todos os dados
 router.get("/",(req,res,next)=>{
-    db.all('SELECT * FROM usuario', (error,rows) => {
-        if(error){
+    db.all('SELECT * FROM usuario', (error, rows) => {
+        if (error) {
             return res.status(500).send({
                 error: error.message
             });
         }
+
         res.status(200).send({
-            mensagem: "aqui estao todos os usuarios",
-            usuarios: row
+            mensagem: "Aqui está a lista de todos os Usuarios",
+            usuarios: rows
         });
     });
-  
+})
+router.get("/login",(req,res,next)=>{
+    const {email,senha} = req.body
+    db.get('SELECT * FROM usuario where email=? and senha=?',[email,senha], (error, rows) => {
+        if (error) {
+            return res.status(500).send({
+                error: error.message
+            });
+        }
+
+        res.status(200).send({
+            mensagem: "Okaerinasai user-sama",
+            usuarios: rows
+        });
+    });
+})
+//consultar apenas um usuario pelo id
+router.get("/:id",(req,res,next)=>{
+    const {id} = req.params;
+    db.get('SELECT * FROM usuario', (error, rows) => {
+        if (error) {
+            return res.status(500).send({
+                error: error.message
+            });
+        }
+
+        res.status(200).send({
+            mensagem: "Aqui está o cadastro do Usuario",
+            usuario: rows
+        });
+    });
+})
+
+// aqui salvamos dados do usuário
+router.post("/",(req,res,next)=>{
+
+ 
 });
 
+// aqui podemos alterar dados do usuário
+router.put("/",(req,res,next)=>{
+    const {id,nome,email,senha} = req.body;
+    db.run('UPDATE usuario SET nome=?,email=?,senha=? where id=?',[nome,email,senha,id], (error, rows) => {
+        if (error) {
+            return res.status(500).send({
+                error: error.message
+            });
+           
+        }
+        res.status(200).send({
+            mensagem: `Usuario de id:${id} foi alterado com sucesso`,
+        });
+    });
+        
 
+});
+ // Aqui podemos deletar o cadastro de um usuário por meio do id
+router.delete("/:id",(req,res,next)=>{
+    const {id} = req.params
+    db.run('DELETE FROM usuario where id=?',[id], (error, rows) => {
+        if (error) {
+            return res.status(500).send({
+                error: error.message
+            });
+           
+        }
+        res.status(200).send({
+            mensagem: `Usuario de id:${id} deletado com sucesso`,
+        });
+    });
+        
+    
 
-
-
-
+   
+});
 module.exports = router;
