bb8b06c7c7cb9ea5607b3b16b9b105ef1416de4c
Carlol6969 <142802832+Carlol6969@users.noreply.github.com>
2024-06-11 11:10:00 -0300

    atualizações de saida e entrada

--- routes/rotaEntrada.js
+++ routes/rotaEntrada.js
@@ -3,12 +3,13 @@ const router = express.Router();
 const sqlite3 = require("sqlite3").verbose();
 const db = new sqlite3.Database("database.db");
 db.run(`CREATE TABLE IF NOT EXISTS 
-        entrada (
+         entrada (
             id INTEGER PRIMARY KEY AUTOINCREMENT, 
-            idproduto integer, 
-            qntde integer, 
-            valorunit real,
-            data date      )
+            idproduto int, 
+            qntde Real, 
+            valorunit Real,
+            dataentrada Date
+            )
             `, (createTableError) => {
     if (createTableError) {
         return res.status(500).send({
@@ -20,7 +21,7 @@ db.run(`CREATE TABLE IF NOT EXISTS
 
 //consultar todos os dados
 router.get("/",(req,res,next)=>{
-    db.all('SELECT * FROM entrada', (error, rows) => {
+db.all('SELECT * FROM entrada', (error, rows) => {
         if (error) {
             return res.status(500).send({
                 error: error.message
@@ -28,16 +29,16 @@ router.get("/",(req,res,next)=>{
         }
 
         res.status(200).send({
-            mensagem: "Aqui está a lista de todas as entradas",
-            produtos: rows
+            mensagem: "Aqui está a lista de todass as Entradas",
+            entradas: rows
         });
     });
 })
 
-//consultar apenas produt pelo id
+//consultar apenas uma entrada pelo id
 router.get("/:id",(req,res,next)=>{
     const {id} = req.params;
-    db.get('SELECT * FROM entrada', (error, rows) => {
+    db.get('SELECT * FROM entrada where id=?',[id], (error, rows) => {
         if (error) {
             return res.status(500).send({
                 error: error.message
@@ -45,86 +46,103 @@ router.get("/:id",(req,res,next)=>{
         }
 
         res.status(200).send({
-            mensagem: "Aqui está o cadastro do Entrada",
-            produto: rows
+            mensagem: "Aqui está o cadastro da Entrada",
+            entrada: rows
         });
     });
 })
 
-// aqui salvamos dados do produto
+
+
+
+// aqui salvamos dados da entrada
 router.post("/",(req,res,next)=>{
-const {idproduto,qntde,valorunit,data} = req.body;
-db.serialize(()=>{ 
-    const insertEntrada = db.prepare(`
-    INSERT INTO entrada(idproduto,qntde,valorunit,data)VALUES(?,?,?,?)`);
-    insertEntrada.run(idproduto,qntde,valorunit,data)
-    insertEntrada.finalize()
-
+    const { idproduto, qntde, valorunit, dataentrada }
+     = req.body;
+   db.serialize(() => {
+        const insertEntrada = db.prepare(`
+        INSERT INTO entrada(idproduto, qntde, valorunit, dataentrada) 
+        VALUES(?,?,?,?)`);
+        insertEntrada.run(idproduto, qntde, valorunit, dataentrada);
+        insertEntrada.finalize();
 });
-atualizarestoque(idproduto,qntde,valorunit);
 
-process.on("SIGINT", ()=>{
-    db.close((err)=>{
-        if (err){
-            return res.status(304).send(err.message);
-        }
-    });
-});
-  res.status(200)
-  .send({mensagem:"Entrada salva com sucesso"
-});
-});
-function atualizarestoque(idproduto,qntde,valorunit,){
-    db.get('SELECT * FROM estoque where idproduto=?',[idproduto], (error, rows) => {
-        if (error) {
-            return res.status(500).send({
-                error: error.message
-            });
-        }
-        if(rows){
-            const qntdestoque = rows.qntde;
-            const novaqntde = parseFloat(qntde)+ parseFloat(qntdestoque)
+            atualizarestoque(idproduto, qntde, valorunit);
 
-
-
-            db.serialize(()=>{ 
-                const updateEstoque = db.prepare(`
-                UPDATE estoque SET qntde=?,valorunit=? Where idproduto=?`);
-                updateEstoque.run(novaqntde,valorunit,idproduto)
-                updateEstoque.finalize()
-            
-            });
-           
-
-        }else{
-            db.serialize(()=>{ 
-                const insertEstoque = db.prepare(`
-                INSERT INTO estoque(idproduto,qntde,valorunit)VALUES(?,?,?)`);
-                insertEstoque.run(idproduto,qntde,valorunit)
-                insertEstoque.finalize()
-            
-            })
-
-        }
-        
-    })
-}
-// aqui podemos alterar dados do produto
-router.put("/",(req,res,next)=>{
-    const {id,idproduto,qntde,valorunit,data} = req.body;
-    db.run('UPDATE entrada SET idproduto=?,qntde=?,valorunit=?,data=? where id=?',[idproduto,qntde,valorunit,data,id], (error, rows) => {
-        if (error) {
-            return res.status(500).send({
-                error: error.message
-            });
-           
-        }
-        res.status(200).send({
-            mensagem: `Entrada de id:${id} foi alterado com sucesso`,
+    process.on("SIGINT", () => {
+        db.close((err) => {
+            if (err) {
+                return res.status(304).send(err.message);
+            }
         });
     });
-        
 
+        
+    res.status(200)
+    .send({ mensagem: "Entrada salvo com sucesso!" });
+});
+
+
+
+
+function atualizarestoque(id_produto,quantidade,valor_unitario){
+   
+    db.get('SELECT * FROM estoque where idproduto=?',
+    [id_produto], (error, rows) => {
+        if (error) {
+            return res.status(500).send({
+                error: error.message
+            });
+        }
+      
+      if(rows){
+        
+        // atualizar a quantidade no estoque 1
+        // acrescentando a quantidade inserida em entrada 1
+        const quantidadeestoque=rows.qntde;
+
+        const quantidadeatualizada=parseFloat(qntde+quantidadeestoque);
+        db.serialize(() => {
+            //const total = quantidade*valor_unitario
+            const updateEstoque = db.prepare(`
+            UPDATE estoque SET  qntde=?, valorunit=? Where idproduto=?`);
+            updateEstoque.run(quantidadeatualizada, valorunit, idproduto);
+            updateEstoque.finalize();
+            
+            
+        });
+        
+      }else{
+        //insira a mesma quantidade inserida em entrada
+        db.serialize(() => {
+            //const total = quantidade*valor_unitario
+            const insertEstoque = db.prepare(`
+            INSERT INTO estoque(idproduto, qntde, valorunit) 
+            VALUES(?,?,?)`);
+            insertEstoque.run(idproduto, qntde, valorunit);
+            insertEstoque.finalize();
+            
+            
+        });
+      }
+
+    });
+}
+// aqui podemos alterar dados da entrada
+router.put("/",(req,res,next)=>{
+    const {id,idproduto, qntde, valorunit, dataentrada} = req.body;
+                db.run(`UPDATE entrada SET 
+                idproduto=?, qntde=?, valorunit=?, dataentrada=?  
+            where id=?`,[idproduto, qntde, valorunit, dataentrada,id], (error, rows) => {
+        if (error) {
+            return res.status(500).send({
+                error: error.message
+            });
+        }
+        res.status(200).send(
+            { mensagem: "Dados da Entrada salvos com sucesso!" 
+            });
+    });
 });
  // Aqui podemos deletar o cadastro de um usuário por meio do id
 router.delete("/:id",(req,res,next)=>{
@@ -134,15 +152,12 @@ router.delete("/:id",(req,res,next)=>{
             return res.status(500).send({
                 error: error.message
             });
-           
         }
-        res.status(200).send({
-            mensagem: `Entrada de id:${id} deletado com sucesso`,
-        });
+        res.status(200).send(
+            { mensagem: "Entrada deletada com sucesso!" 
+            });
     });
-        
-    
 
-   
+
 });
 module.exports = router;
--- routes/rotaSaida.js
+++ routes/rotaSaida.js
@@ -3,12 +3,13 @@ const router = express.Router();
 const sqlite3 = require("sqlite3").verbose();
 const db = new sqlite3.Database("database.db");
 db.run(`CREATE TABLE IF NOT EXISTS 
-        saida (
+         saida (
             id INTEGER PRIMARY KEY AUTOINCREMENT, 
-            idproduto integer, 
-            qntde integer, 
-            valorunit real,
-            datasaida date      )
+            idproduto int, 
+            qntde Real, 
+            valorunit Real,
+            datasaida Date
+            )
             `, (createTableError) => {
     if (createTableError) {
         return res.status(500).send({
@@ -20,7 +21,7 @@ db.run(`CREATE TABLE IF NOT EXISTS
 
 //consultar todos os dados
 router.get("/",(req,res,next)=>{
-    db.all('SELECT * FROM saida', (error, rows) => {
+db.all('SELECT * FROM saida', (error, rows) => {
         if (error) {
             return res.status(500).send({
                 error: error.message
@@ -28,16 +29,16 @@ router.get("/",(req,res,next)=>{
         }
 
         res.status(200).send({
-            mensagem: "Aqui está a lista de todas as saídas",
-            produtos: rows
+            mensagem: "Aqui está a lista de todas as Saídas",
+            saidas: rows
         });
     });
 })
 
-//consultar apenas produt pelo id
+//consultar apenas uma saída pelo id
 router.get("/:id",(req,res,next)=>{
     const {id} = req.params;
-    db.get('SELECT * FROM saida', (error, rows) => {
+    db.get('SELECT * FROM saida where id=?',[id], (error, rows) => {
         if (error) {
             return res.status(500).send({
                 error: error.message
@@ -46,86 +47,102 @@ router.get("/:id",(req,res,next)=>{
 
         res.status(200).send({
             mensagem: "Aqui está o cadastro da Saída",
-            produto: rows
+            saida: rows
         });
     });
 })
 
-// aqui salvamos dados do produto
+
+
+
+// aqui salvamos dados da Saída
 router.post("/",(req,res,next)=>{
-const {idproduto,qntde,valorunit,datasaida} = req.body;
-db.serialize(()=>{ 
-    const insertSaida = db.prepare(`
-    INSERT INTO saida(idproduto,qntde,valorunit,datasaida)VALUES(?,?,?,?)`);
-    insertSaida.run(idproduto,qntde,valorunit,datasaida)
-    insertSaida.finalize()
-
+    const { idproduto, qntde, valorunit, datasaida }
+     = req.body;
+   db.serialize(() => {
+        const insertSaida = db.prepare(`
+        INSERT INTO saida(idproduto, qntde, valorunit, datasaida) 
+        VALUES(?,?,?,?)`);
+        insertSaida.run(idproduto, qntde, valorunit, datasaida);
+        insertSaida.finalize();
 });
-atualizarestoque(idproduto,qntde,valorunit);
-process.on("SIGINT", ()=>{
-    db.close((err)=>{
-        if (err){
-            return res.status(304).send(err.message);
-        }
-    });
-});
-  res.status(200)
-  .send({mensagem:"Saída salva com sucesso"
-});
-});
-function atualizarestoque(idproduto,qntde,valorunit,){
-    db.get('SELECT * FROM estoque where idproduto=?',[idproduto], (error, rows) => {
-        if (error) {
-            return res.status(500).send({
-                error: error.message
-            });
-        }
-        if(rows){
-            const qntdestoque = rows.qntde;
-            const novaqntde = parseInt(qntdestoque) - parseInt(qntde) 
 
+            atualizarestoque(idproduto,qntde);
 
-
-            db.serialize(()=>{ 
-                const updateEstoque = db.prepare(`
-                UPDATE estoque SET qntde=?,valorunit=? Where idproduto=?`);
-                updateEstoque.run(novaqntde,valorunit,idproduto)
-                updateEstoque.finalize()
-            
-            });
-           
-
-        }else{
-            // db.serialize(()=>{ 
-            //     const insertEstoque = db.prepare(`
-            //     INSERT INTO estoque(idproduto,qntde,valorunit)VALUES(?,?,?)`);
-            //     insertEstoque.run(idproduto,qntde,valorunit)
-            //     insertEstoque.finalize()
-            
-            // })
-
-        }
-        
-    })
-}
-// aqui podemos alterar dados do produto
-router.put("/",(req,res,next)=>{
-    const {id,idproduto,qntde,valorunit,datasaida} = req.body;
-    db.run('UPDATE saida SET idproduto=?,qntde=?,valorunit=?,datasaida=? where id=?',[idproduto,qntde,valorunit,datasaida,id], (error, rows) => {
-        if (error) {
-            return res.status(500).send({
-                error: error.message
-            });
-           
-        }
-        res.status(200).send({
-            mensagem: `Saída de id:${id} foi alterado com sucesso`,
+    process.on("SIGINT", () => {
+        db.close((err) => {
+            if (err) {
+                return res.status(304).send(err.message);
+            }
         });
     });
-        
 
+        
+    res.status(200)
+    .send({ mensagem: "Entrada salvo com sucesso!" });
 });
- // Aqui podemos deletar o cadastro de um usuário por meio do id
+
+
+
+
+function atualizarestoque(idproduto,qntde){
+   
+    db.get('SELECT * FROM estoque where idproduto=?',
+    [idproduto], (error, rows) => {
+        if (error) {
+            return res.status(500).send({
+                error: error.message
+            });
+        }
+      
+      if(rows){
+        
+        // atualizar a quantidade no estoque 1
+        // acrescentando a quantidade inserida em entrada 1
+        const quantidadeestoque=rows.qntde;
+         if (quantidadeestoque<qntde || quantidadeestoque===0){
+                 return res.status(404).send({
+                  mensagem: "Quantidade é menor que a quantidade no estoque"
+                    });
+        }
+        const quantidadeatualizada=parseFloat(quantidadeestoque-qntde);
+        db.serialize(() => {
+            //const total = quantidade*valor_unitario
+            const updateEstoque = db.prepare(`
+            UPDATE estoque SET  qntde=? Where idproduto=?`);
+            updateEstoque.run(quantidadeatualizada, idproduto);
+            updateEstoque.finalize();
+            
+            
+        });
+        
+      }else{
+        //produto nao encontrado
+        return res.status(404).send({
+            mensagem: "Produto nao foi encontrado no estoque"
+        });
+      }
+
+    });
+}
+// aqui podemos alterar dados da saida
+router.put("/",(req,res,next)=>{
+    const {id,id_produto, quantidade, valor_unitario,data_saida} = req.body;
+                db.run(`UPDATE entrada SET 
+                idproduto=?, qntde=?, valorunit=?, datasaida=?
+            where id=?`,[idproduto, qntde, valorunit, datasaida], 
+            (error, rows) => {
+        if (error) {
+            return res.status(500).send({
+                error: error.message
+            });
+        }
+        res.status(200).send(
+            { mensagem: "Dados da Saída salvos com sucesso!" 
+            });
+    });
+});
+ // Aqui podemos deletar o cadastro de um saída por meio do id
 router.delete("/:id",(req,res,next)=>{
     const {id} = req.params
     db.run('DELETE FROM saida where id=?',[id], (error, rows) => {
@@ -133,15 +150,12 @@ router.delete("/:id",(req,res,next)=>{
             return res.status(500).send({
                 error: error.message
             });
-           
         }
-        res.status(200).send({
-            mensagem: `Saída de id:${id} deletado com sucesso`,
-        });
+        res.status(200).send(
+            { mensagem: "Saída deletada com sucesso!" 
+            });
     });
-        
-    
 
-   
+
 });
 module.exports = router;
